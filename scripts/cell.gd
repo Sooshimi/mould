8103e733247a22a05844fc959bbf8673acaf1e45
Sooshimi <59628017+Sooshimi@users.noreply.github.com>
2025-09-15 12:55:58 +0100

    Update cell.gd

--- scripts/cell.gd
+++ scripts/cell.gd
@@ -8,23 +8,23 @@ var left_infected := false
 var right_infected := false
 var cell_infected := false
 
-func is_infected() -> void:
+func checked_if_infected() -> void:
 	if top_infected and bottom_infected and left_infected and right_infected:
 		cell_infected = true
 		infected_indicator.show()
 
 func _on_top_body_entered(body: CharacterBody2D) -> void:
 	top_infected = true
-	is_infected()
+	checked_if_infected()
 
 func _on_bottom_body_entered(body: CharacterBody2D) -> void:
 	bottom_infected = true
-	is_infected()
+	checked_if_infected()
 
 func _on_left_body_entered(body: CharacterBody2D) -> void:
 	left_infected = true
-	is_infected()
+	checked_if_infected()
 
 func _on_right_body_entered(body: CharacterBody2D) -> void:
 	right_infected = true
-	is_infected()
+	checked_if_infected()
