6ef10ccba659dee09d7c9a1c7e68e233d6cb6ef4
Sooshimi <59628017+Sooshimi@users.noreply.github.com>
2025-09-16 21:47:22 +0100

    Update SceneManager.gd

--- scripts/globals/SceneManager.gd
+++ scripts/globals/SceneManager.gd
@@ -1,3 +1,13 @@
 extends Node
 
+enum Phase {GAME_TUTORIAL, GAME_LEVEL_1}
+
+var current_phase: int = Phase.GAME_TUTORIAL
+
 signal start_button_click
+
+func change_to_state(state: int) -> void:
+	if state == 0:
+		current_phase = Phase.GAME_TUTORIAL
+	if state == 1:
+		current_phase = Phase.GAME_LEVEL_1
