fa253ac4b858e54917cea4930f2304c401798438
Sooshimi <59628017+Sooshimi@users.noreply.github.com>
2025-09-20 21:08:29 +0100

    Add main loop music

--- scripts/main.gd
+++ scripts/main.gd
@@ -2,6 +2,10 @@ extends Node
 
 func _ready() -> void:
 	SceneManager.start_button_click.connect(show_game)
+	$MainLoopMusic.play()
 
 func show_game() -> void:
 	$Game.show()
+
+func _on_main_loop_music_finished():
+	$MainLoopMusic.play()
