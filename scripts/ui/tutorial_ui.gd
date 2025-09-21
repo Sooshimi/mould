30211088c2aa5a7772723dfddac94b4c978d6c3e
Sooshimi <59628017+Sooshimi@users.noreply.github.com>
2025-09-21 20:17:08 +0100

    update

--- scripts/ui/tutorial_ui.gd
+++ scripts/ui/tutorial_ui.gd
@@ -4,7 +4,7 @@ const dialogue = [
 	"",
 	"In this game, you play as a slime which spreads fungus!\n\nTry moving with WASD or the arrow keys!",
 	"Great! Did you notice the top green HP bar go down? It goes down by 1 HP every time you move, so watch your HP!\n\nFor every 6 moves you make, humans on the board will move 1 square too.\n\nYou can infect a piece of the ground by moving around all its edges. Try it!",
-	"Nice job, you just infected your first bit of ground! This is also indicated by the pink bar at the top, just below your HP bar.\n\nThe aim of the game is to infect 75% of the ground. You can also infect humans by moving around the uninfected ground they're stood on. Infected humans can spread the infection, so they will be useful!\n\nTry infecting another ground, or a human!",
+	"Nice job, you just infected your first bit of ground! This is also indicated by the pink bar at the top, just below your HP bar.\n\nThe aim of the game is to infect 50% of the ground. You can also infect humans by moving around the uninfected ground they're stood on. Infected humans can spread the infection, so they will be useful!\n\nTry infecting another ground, or a human!",
 	"Perfect. There's a few more things to mention:\n\n- Wolves can kill humans. You can kill wolves by moving around the ground they're stood on.\n\n- You can gain HP and max HP by infecting humans, and gain HP for every 3 cells infected!"
 ]
 
